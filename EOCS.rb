f1c953d87e883b99c436c54347df84f3138d3902
Elyse Dougherty <elysedougherty@gmail.com>
2013-06-25 10:38:45 -0300

    Update EOCS.rb

--- EOCS.rb
+++ EOCS.rb
@@ -1,11 +1,11 @@
-require ‘soap/wsdlDriver’
+require 'soap/wsdlDriver'
 
 def XML
     # declare and build String containing XML
-    _xmlString = “<KnowWho><Customer> ... </Customer><FormData> ... </FormData></KnowWho>”;
+    _xmlString = "<KnowWho><Customer> ... </Customer><FormData> ... </FormData></KnowWho>";
 
     # declare new instance of the service and call the appropriate function, passing the string
-    _returnedXml = SOAP::WSDLDriverFactory.new(‘http://knowwho.info/Services/ElectedOfficialsCommunicationsService.asmx?WSDL’).GetService({ ‘InputString’ => _xmlString }).GetServiceResult;
+    _returnedXml = SOAP::WSDLDriverFactory.new("http://knowwho.info/Services/ElectedOfficialsCommunicationsService.asmx?WSDL").GetService({ 'InputString' => _xmlString }).GetServiceResult;
 
     # if all was successful, returnedXml now contains a string representation of the returned XML
 end
@@ -15,7 +15,7 @@ def JSON
     _jsonString = "{ KnowWho: { Customer: { ... }, FormData: { ... } } }";
 
     # declare new instance of the service and call the appropriate function, passing the string
-    _returnedJson = SOAP::WSDLDriverFactory.new(‘http://knowwho.info/Services/ElectedOfficialsCommunicationsService.asmx?WSDL’).GetService({ ‘InputString’ => _jsonString }).GetServiceResult;
+    _returnedJson = SOAP::WSDLDriverFactory.new("http://knowwho.info/Services/ElectedOfficialsCommunicationsService.asmx?WSDL").GetService({ 'InputString' => _jsonString }).GetServiceResult;
 
     # if all was successful, returnedJson now contains a string representation of the returned JSON
 end
